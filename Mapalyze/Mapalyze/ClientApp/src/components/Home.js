cbbf55df46b3823d6851a4359a414d08102b7f58
Asrar <81701822+Asrarfarooq@users.noreply.github.com>
2023-09-29 18:10:01 -0500

    Revert "Merge branch 'main' into origin/Asrar_working_branch"
    This reverts commit 4b1630c409c801529bda255068f1005a1e7076d7, reversing
    changes made to 4da010a19c2f9e138c93b5e7bc531fdfcacf97be.

--- Mapalyze/Mapalyze/ClientApp/src/components/Home.js
+++ Mapalyze/Mapalyze/ClientApp/src/components/Home.js
@@ -1,22 +1,11 @@
 import React, { Component } from 'react';
 import Conversioneg from './HomePageComponents/conversioneg';
-import Videocomponent from './HomePageComponents/videocomponent';
-import Example from './HomePageComponents/component';
-import classes from './Home.module.css';
 export class Home extends Component {
   static displayName = Home.name;
 
   render() {
     return (
-
-      <div className={classes.home}>
-          <Conversioneg/>
-          <Videocomponent/>
-          <Example/>
-          <Example/>
-          <Example/>
-      </div>
-     
+     <Conversioneg/>
     );
   }
 }

b31d04972620665dfa09895760920f67f73a6ad4
Asrar <81701822+Asrarfarooq@users.noreply.github.com>
2023-09-28 19:42:54 -0500

    added login sigup

--- Mapalyze/Mapalyze/ClientApp/src/App.js
+++ Mapalyze/Mapalyze/ClientApp/src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, Routes } from "react-router-dom";
 //import AppRoutes from './AppRoutes';
-import { Layout } from './components/Layout';
-import './custom.css';
-import Home from './components/Home';
-
-import AboutUs from './components/AboutUs';
-import AppInterface from './components/AppInterface';
+import { Layout } from "./components/Layout";
+import "./custom.css";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import AboutUs from "./components/AboutUs";
+import AppInterface from "./components/AppInterface";
+import Signup from "./components/SignUp";
 export default class App extends Component {
   static displayName = App.name;
 
@@ -14,9 +15,11 @@ export default class App extends Component {
     return (
       <Layout>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/AboutUs" element={<AboutUs/>}/>
-          <Route path="/App" element={<AppInterface/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/App" element={<AppInterface />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Layout>
     );

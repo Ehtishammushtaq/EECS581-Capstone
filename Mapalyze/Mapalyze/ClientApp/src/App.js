2d5fd50ba10518a6de3c9b0b7e268adf0624f43b
Ehtisham Mushtaq <ehtishammushtaq@gmail.com>
2023-09-29 17:17:55 -0500

    Revert "Merge remote-tracking branch 'origin/origin/Asrar_working_branch' into Eht_working_branch"
    This reverts commit f6d02256727b3832974f2f0f1c2e0fd9485904a2, reversing
    changes made to ae185a13762e78f22df92be0ff62e7405145ec54.

--- Mapalyze/Mapalyze/ClientApp/src/App.js
+++ Mapalyze/Mapalyze/ClientApp/src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
 //import AppRoutes from './AppRoutes';
-import { Layout } from "./components/Layout";
-import "./custom.css";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import AboutUs from "./components/AboutUs";
-import AppInterface from "./components/AppInterface";
-import Signup from "./components/SignUp";
+import { Layout } from './components/Layout';
+import './custom.css';
+import Home from './components/Home';
+
+import AboutUs from './components/AboutUs';
+import AppInterface from './components/AppInterface';
 export default class App extends Component {
   static displayName = App.name;
 
@@ -17,11 +16,9 @@ export default class App extends Component {
 
         <Layout/>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/AboutUs" element={<AboutUs />} />
-          <Route path="/App" element={<AppInterface />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/AboutUs" element={<AboutUs/>}/>
+          <Route path="/App" element={<AppInterface/>}/>
         </Routes>
      
       </div>

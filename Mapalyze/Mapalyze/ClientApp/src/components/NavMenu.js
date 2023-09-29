43dc85d110962ea44429ff40a17cfc7ea11f0d15
Ehtisham Mushtaq <ehtishammushtaq@gmail.com>
2023-09-29 18:10:19 -0500

    Revert "Revert "Merge branch 'main' into origin/Asrar_working_branch""
    This reverts commit dcfecab87fc770947ec89e3f006d4daf4064876e.

--- Mapalyze/Mapalyze/ClientApp/src/components/NavMenu.js
+++ Mapalyze/Mapalyze/ClientApp/src/components/NavMenu.js
@@ -1,71 +1,58 @@
-import React, { Component } from "react";
-import {
-  Collapse,
-  Navbar,
-  NavbarBrand,
-  NavbarToggler,
-  NavItem,
-  NavLink,
-} from "reactstrap";
-import { Link } from "react-router-dom";
-import "./NavMenu.css";
-import AppInterface from "./AppInterface";
+import React, { Component } from 'react';
+import { Collapse, Navbar, Nav,  NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import classes from './NavMenu.module.css';
+
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
 
-  constructor(props) {
+  constructor (props) {
     super(props);
 
     this.toggleNavbar = this.toggleNavbar.bind(this);
     this.state = {
-      collapsed: true,
+      collapsed: true
     };
   }
 
-  toggleNavbar() {
+  toggleNavbar () {
     this.setState({
-      collapsed: !this.state.collapsed,
+      collapsed: !this.state.collapsed
     });
   }
 
   render() {
+    const { collapsed } = this.state;
+
     return (
-      <header>
-        <Navbar
-          className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3"
-          container
-          light
-        >
-          <NavbarBrand tag={Link} to="/">
-            Mapalyze
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse
-            className="d-sm-inline-flex flex-sm-row-reverse"
-            isOpen={!this.state.collapsed}
-            navbar
-          >
-            <ul className="navbar-nav flex-grow">
+      <header className={classes.navmenu}>
+        <Navbar expand="sm" light>
+          <div className={classes.mainlogo}>
+            <NavbarBrand tag={Link} to="/">Mapalyze</NavbarBrand>
+          </div>
+
+          <NavbarToggler onClick={this.toggleNavbar} />
+
+          <Collapse isOpen={!collapsed} navbar>
+           <div className='ml-auto '>
+            <Nav navbar>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">
-                  Home
-                </NavLink>
+                <NavLink tag={Link} to="/">Home</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/AboutUs">
-                  About Us
-                </NavLink>
+                <NavLink tag={Link} to="/AboutUs">About Us</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/login">
-                  Login
-                </NavLink>
+                <NavLink tag={Link} to="/App">Login</NavLink>
               </NavItem>
-            </ul>
+            </Nav>
+            </div> 
           </Collapse>
         </Navbar>
       </header>
     );
   }
 }
+
+export default NavMenu;

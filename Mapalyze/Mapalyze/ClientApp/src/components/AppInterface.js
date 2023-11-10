5787756beb954b42be3bdeaa91b6645e4aad0101
Ehtisham Mushtaq <ehtishammushtaq@gmail.com>
2023-11-10 16:31:55 -0600

    React planner integration

--- Mapalyze/Mapalyze/ClientApp/src/components/AppInterface.js
+++ Mapalyze/Mapalyze/ClientApp/src/components/AppInterface.js
@@ -46,7 +46,9 @@ export class AppInterface extends Component {
         <img src = {Dummyrender} alt="Dummy render" width="700" height="500"></img>
         <br/>
         <br/>
-        <button>Render (dummy button)</button>
+        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-5">
+        <a href="https://asrarfarooq.github.io/Mapalyze/"  rel="noopener noreferrer">Render (dummy button)</a>
+      </button>
         <br/>
 
         {/* Hidden file input element */}
